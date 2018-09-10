1be233dbcad186b4839bd4e86de6d0b695298336
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-10 02:34:05 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+require 'benchmark'  # => true
+
+def prime? int
+  max = math.sqrt(int)
+  (2..9)
+end
+
+(2..9).to_a
+
+# ~> LoadError
+# ~> cannot load such file -- math
+# ~>
+# ~> /usr/local/Cellar/ruby/2.5.1/lib/ruby/2.5.0/rubygems/core_ext/kernel_require.rb:59:in `require'
+# ~> /usr/local/Cellar/ruby/2.5.1/lib/ruby/2.5.0/rubygems/core_ext/kernel_require.rb:59:in `require'
+# ~> prime.rb:2:in `<main>'
