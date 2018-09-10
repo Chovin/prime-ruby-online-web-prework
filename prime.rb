1aabc76af24e39de3f4e2b7b1c0d3faa16e60923
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-10 03:05:28 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -6,7 +6,9 @@ def prime? int
   return true if int <= 3
   return false if int % 2 == 0 || int % 3 ==0
   max = Math.sqrt(int)
-  
+  (5..max).step(6).each do |i|
+    return false if int % i == 0 || int % (i + 2) == 0
+  end
 end                     # => :prime?
 
 (2..9).map {|x| Math.sqrt x}  # => [1.4142135623730951, 1.7320508075688772, 2.0, 2.23606797749979, 2.449489742783178, 2.6457513110645907, 2.8284271247461903, 3.0]
