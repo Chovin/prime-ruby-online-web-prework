296eb1327efb650ef467620f0229563f5fb9164e
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-10 02:55:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,12 @@
 require 'benchmark'  # => true
 
+# https://en.wikipedia.org/wiki/Primality_test 6k+1
 def prime? int
-  max = math.sqrt(int)
-  (2..9)
+  return false if int < 1
+  return true if int <= 3
+  return false if int % 2 == 0 || int % 3 ==0
+  max = Math.sqrt(int)
+  
 end                     # => :prime?
 
-(2..9).map {|x| Math.sqrt x}  # ~> NoMethodError: undefined method `sqrt' for main:Object
-
-# ~> NoMethodError
-# ~> undefined method `sqrt' for main:Object
-# ~>
-# ~> prime.rb:8:in `block in <main>'
-# ~> prime.rb:8:in `each'
-# ~> prime.rb:8:in `map'
-# ~> prime.rb:8:in `<main>'
+(2..9).map {|x| Math.sqrt x}  # => [1.4142135623730951, 1.7320508075688772, 2.0, 2.23606797749979, 2.449489742783178, 2.6457513110645907, 2.8284271247461903, 3.0]
